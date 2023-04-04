9fcc50a0d83a6734e0ac505be98ec2f85582d0b2
limchaeyeon8 <2000goal2001@gmail.com>
2023-04-04 12:16:30 +0900

    c# 첫 커

---
+++ d1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+// 네임스페이스 ConsoleApp1
+namespace ConsoleApp1
+{
+    /// <summary> xml 주석 (문서화 자동화 가능)
+    /// 프로그램 클래스
+    /// </summary>
+    internal class Program      // 프로그램파일명과 클래스명을 같게 쓰는 걸 권장
+    {
+        /// <summary>
+        /// * 메인 엔트리 포인트
+        /// </summary>
+        /// <param name="args">콘솔 매개변수</param>
+        static void Main(string[] args)     // static; 정적함수(메소드) // string 배열에 arguments(여러개 값 저장)
+        {
+            Console.WriteLine("Hello C#!!");
+        }
+    }
+}
